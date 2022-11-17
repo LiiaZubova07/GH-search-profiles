7d181a54f1082197d1c187f0febb06ae301a548d
Liia Zubova <vardlitmo@gmail.com>
2022-11-17 18:24:08 +0300

    add: response

--- js/main.js
+++ js/main.js
@@ -16,18 +16,24 @@ const mainEl = document.querySelector('.main');
 
 const formEl = document.createElement('form');
 formEl.classList.add('search');
-formEl.addEventListener('submit', (e) => {
-	//сбросить стандартное поведение в браузере
-e.preventDefault();
+formEl.addEventListener('submit', async (e) => {
+  //сбросить стандартное поведение в браузере
+  e.preventDefault();
 
-//забираю значение из этого ипута, которое буду вводить
-//позволяет забрать неограниченное количество значений из формы
-const inputsValue = Object.fromEntries(new FormData(e.target));
+  //забираю значение из этого ипута, которое буду вводить
+  //позволяет забрать неограниченное количество значений из формы
+  const inputsValue = Object.fromEntries(new FormData(e.target));
+  //сетевой запрос
+  const response = await fetch(`https://api.github.com/users/${inputsValue.name}`);
+ if(response.ok) {
+  const data = await response.json();
+ }
+  console.log(response);
 });
 
 const inputEl = document.createElement('input');
 inputEl.classList.add('search-input');
-// inputEl.setAttribute('name', 'name');
+inputEl.setAttribute('name', 'name');
 
 const searchButtonEl = document.createElement('button');
 searchButtonEl.classList.add('search-button');
@@ -41,5 +47,3 @@ formEl.appendChild(inputEl);
 formEl.appendChild(searchButtonEl);
 //добавить форму к main
 mainEl.appendChild(formEl);
-
-
